937c192061c4e5e1b319babfb49a3df974d36a6c
Abdul Rafay Modi <74718326+AlphaRomeoMike@users.noreply.github.com>
2022-02-20 00:58:31 +0500

    Added lectures for class two 🚀🚀

---
+++ ClassTwo/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ClassTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Reference of a class, local variable, sets a space in the memory
+            Student student = new Student();
+            Student student2 = new Student();
+            Console.WriteLine("Code ran");
+        }
+    }
+
+    class Student
+    {
+        public string Name;
+        public int Age;
+        public string Class;
+        public float CGPA;
+    }
+}
