7ccb0214679e1e9f1f769874f93689eee9c6a884
Abdul Rafay Modi <74718326+AlphaRomeoMike@users.noreply.github.com>
2022-05-07 23:48:28 +0500

    Add project files.

---
+++ Application/App.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    class App
+    {
+        public static string Name = "Banking Application";
+
+        public static string Description = "Welcome to banking application";
+
+        public static void Welcome()
+        {
+            Console.WriteLine($"{Name}\n{Description}");
+        }
+    }
+}
---
+++ Application/BankAccount.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    internal class BankAccount : Transaction
+    {
+        public string Name;
+        public int Number;
+        public decimal Balance;
+        public decimal LastTransactionAmount;
+        public DateTime LastTransactionTime;
+        public BankAccount(string Name, int Number)
+        {
+            Name = Name;
+            Number = Number;
+            Balance = 0;
+        }
+
+        public BankAccount(string Name, int Number, decimal Amount)
+        {
+            Name = Name;
+            Number = Number;
+            Balance = Amount;
+        }
+
+        public override void makeWithdrawal(string Name, DateTime time, decimal AmountAvailable)
+        {
+            if (AmountAvailable <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(AmountAvailable), "Amount of withdraw");
+            }
+
+            if (Balance - AmountAvailable <= 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds");
+            }
+
+            Balance = Balance - AmountAvailable;
+            LastTransactionAmount = AmountAvailable;
+            LastTransactionTime = time;
+        }
+
+        public string getDetails()
+        {
+            return $"The account balance is {Balance} and account holder\'s name is {Name}";
+        }
+
+        public string getLastTransaction()
+        {
+            return $"The last transaction was made on {LastTransactionTime} of of amount {LastTransactionAmount}";
+        }
+    }
+}
